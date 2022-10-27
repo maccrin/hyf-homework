0464359dc6537e8787ac5638fc864ce05f562a00
maccrin <maccrin@gmail.com>
2022-10-27 16:14:47 +0200

    fixed the bug

--- javascript/javascript1/week2/weather.js
+++ javascript/javascript1/week2/weather.js
@@ -1,18 +1,24 @@
 function clothesToWear(temp) {
-    if (temp <= 0) {
-        alert("Please wear a  Bomber jacket as snow fall is continuing here in Demark!!");
+    if (isNaN(temp)) {
+        console.log(`Please provide a valid input`);
+        return;
     }
-    else if (temp <= 10) {
-        alert("Please wear a proper winter jacket it's too cold here in Demark!!");
+    else {
+        if (temp <= 0) {
+            console.log("Please wear a  Bomber jacket as snow fall is continuing here in Demark!!");
+        }
+        else if (temp <= 10) {
+            console.log("Please wear a proper winter jacket it's too cold here in Demark!!");
+        }
+        else if (temp > 10 && temp <= 15) {
+            console.log("Please wear a  Light jacket as its Autumn here in Demark!!");
+        }
+        else if (temp <= 25 && temp > 15) {
+            console.log("Please wear a  sweatshirt as its windy  here in Demark!!");
+        }
+        else
+            console.log("Oh temerature is rising  lets try some summer cool clothings!");
     }
-    else if (temp > 10 && temp <= 15) {
-        alert("Please wear a  Light jacket as its Autumn here in Demark!!");
-    }
-    else if (temp <= 25 && temp > 15) {
-        alert("Please wear a  sweatshirt as its windy  here in Demark!!");
-    }
-    else
-        alert("Oh temerature is rising  lets try some summer cool clothings!");
 
 }
 
