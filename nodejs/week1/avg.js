11d9232319539d5241cf88f0ef84e31e51dc2272
maccrin <maccrin@gmail.com>
2023-01-30 11:32:42 +0100

    warm up file

---
+++ nodejs/week1/avg.js
@@ -0,0 +1,21 @@
+(function () {
+    try {
+        if (process.argv.slice(2).length) {
+            const numericInput = process.argv.slice(2).filter(x => (!isNaN(x))).length;
+            if (numericInput) {
+                const sum = process.argv.slice(2).map(x => (parseFloat(x))).filter(x => !isNaN(x)).reduce((a, b) => a + b)
+                const avg = parseFloat(sum / numericInput);
+                console.log(`average of numeric elements are ${avg}`)
+            }
+            else {
+                throw new Error('provide some numeric values to calculate avg')
+            }
+        }
+        else
+            throw new Error(`Please Provide Arguments`)
+    }
+    catch (e) {
+        console.log(e.message)
+        return;
+    }
+})();
