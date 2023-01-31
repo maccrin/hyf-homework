a18d73bdeb4b8797e342c8c172c14d89ec37a9da
maccrin <maccrin@gmail.com>
2023-01-31 13:54:04 +0100

    I modififed this file for better logic

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -3,7 +3,7 @@
         if (process.argv.slice(2).length) {
             const numericInput = process.argv.slice(2).filter(x => (!isNaN(x))).length;
             if (numericInput) {
-                const sum = process.argv.slice(2).map(x => (parseFloat(x))).filter(x => !isNaN(x)).reduce((a, b) => a + b)
+                const sum = process.argv.slice(2).filter(x => (!isNaN(x))).map(x => parseFloat(x)).reduce((a, b) => a + b)
                 const avg = parseFloat(sum / numericInput);
                 console.log(`average of numeric elements are ${avg}`)
             }
