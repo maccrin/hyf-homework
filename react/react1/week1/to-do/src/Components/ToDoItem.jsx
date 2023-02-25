e32cb0ecfb71545a75dc1a144592ac0c87852539
maccrin <maccrin@gmail.com>
2023-02-25 15:15:39 +0100

    fixed as per review

--- react/react1/week1/to-do/src/Components/ToDoItem.jsx
+++ react/react1/week1/to-do/src/Components/ToDoItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 function ToDoItem (value) {
+    console.log(value)
+    const {taskItem}=value
     return (
         <li className="list">
-            <p>Title: {value.task}</p>
-            <p className="deadline">DeadLine: {value.deadline}</p>
+            <p>Title: {taskItem.task}</p>
+            <p className="deadline">DeadLine: {taskItem.deadLine}</p>
         </li>
     )
 }
