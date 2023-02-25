d2ec1fa7b6424eb645b985526254e648193a843c
maccrin <maccrin@gmail.com>
2023-02-25 15:15:56 +0100

    fixed as per review comment

--- react/react1/week1/to-do/src/Components/ToDoList.jsx
+++ react/react1/week1/to-do/src/Components/ToDoList.jsx
@@ -1,6 +1,6 @@
 
 import ToDoItem from './ToDoItem'
-import ToDoList from './ToDoHeader'
+import ToDoListHeader from './ToDoHeader'
 const listArray = [
   {  task: "Get out of bed", deadLine: "Wed Sep 13 2017" },
   { task: "Brush teeth", deadLine: "Thu Sep 14 2017" },
@@ -10,26 +10,14 @@ const listArray = [
 function ToDo() {
   return (
     <div className="header">
-      <ToDoList/>
-      <ItemKey/>
+      <ToDoListHeader/>
+      {<ul className="list">
+{listArray.map((eachList,i)=> {
+ return <ToDoItem taskItem={eachList} key={i}/>
+})}</ul>}
       </div>  
   );
 }
-function ItemKey(){
-return (
-    <ul className="list">
-{listArray.map((eachList,i)=> {
- return <TaskItem taskItem={eachList} key={i}/>
-})}
-    </ul>
-)
-}
-function TaskItem({taskItem}){
-return (
-  <div className="eachitem">
-<ToDoItem task={taskItem.task}
-deadline={taskItem.deadLine}/> 
-</div>)
-}
+
 
 export default ToDo
